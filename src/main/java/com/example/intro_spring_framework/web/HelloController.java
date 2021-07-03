42248ea2914f9cadf90f5a74d5e6fbb534b07c9b
Ivan-Tashev <anl@abv.bg>
2021-07-04 01:54:07 +0300

    Swagger config added.

--- src/main/java/com/example/intro_spring_framework/web/HelloController.java
+++ src/main/java/com/example/intro_spring_framework/web/HelloController.java
@@ -7,19 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/hello") // localhost:8080/hello - endpoint path
     public String sayHello(@RequestParam(value = "name", required = false, defaultValue = "Unknown") String name){
             // http://localhost:8080/hello?name=Ivan
         return String.format("Hello %s, from Spring MVC!", name);
     }
-
     @GetMapping("/users/{userId}/posts/{postId}") // template/path parameter
     public String getPost(@PathVariable Integer userId, @PathVariable Integer postId){
             // http://localhost:8080/users/10/posts/15
         return String.format("User: %d --> Post: %d%n", userId, postId);
     }
-
-
-
 }
